105b88b081bb39b032f2d4640e8af66912c9e036
LeeJunGyun <rainmaker0303@hanmail.net>
2023-03-04 15:32:56 +0900

    #26 add logging

--- coupon-issuance-request/core/core-api/src/main/kotlin/io/simple/coupon/core/api/controller/example/RedisExampleController.kt
+++ coupon-issuance-request/core/core-api/src/main/kotlin/io/simple/coupon/core/api/controller/example/RedisExampleController.kt
@@ -1,6 +1,7 @@
 package io.simple.coupon.core.api.controller.example
 
 import io.simple.coupon.storage.redis.core.SortedSetExampleRedisRepository
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -9,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController
 class RedisExampleController(
     val sortedSetExampleRedisRepository: SortedSetExampleRedisRepository
 ) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
     @PostMapping("/example/sortedSet")
     fun addRedisSortedSetExample(
         @RequestBody request: RedisExampleDto.AddRequest
     ) {
+        log.info("addRedisSortedSetExample request : {}", request)
         sortedSetExampleRedisRepository.addValueToSortedSet(request.key, request.value)
     }
 }
